a27ee8847bdf5274d07f277310ece26894c17c40
Administrator <bward6700@gmail.com>
2022-04-22 14:00:27 -0400

    coding challenge

---
+++ coding challenge apr 22/java.js
@@ -0,0 +1,30 @@
+// Create 3 arrays, One with all String data types, one with all Numbers data types, 
+// and one with Boolean data types.  You will need to add sample data to each array (don't over think this). 
+// Each array should have at least 5 elements.. 
+
+// 2) Next create a function that can take any of these arrays as a parameter and add an additional 
+// String, Number, or Boolean depending on which array was passed in. 
+
+let entry = prompt("choose strings,numbers or booleans");
+
+let strings = ["hello", "hi","hey", "howdy", "yo"];
+let numbers = [1,2,3,4,5];
+let booleans = [ 2<3, 7>6, 8>7, 28<29, 0<200];
+
+ function apply(strings, numbers, booleans){
+     if(prompt.typeof = strings){
+         strings.push("good morning");
+     }
+
+     else if(prompt.typeof = numbers){
+        numbers.push("6");
+     }
+
+        else{
+            booleans.push("5=5");
+ }
+
+ }
+
+
+ 
